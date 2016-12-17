857734dd019e121c8c76c61cc98ea59d817af391
jayherring <j.herring.8989@gmail.com>
2016-12-17 16:06:13 -0600

    attr_accessor

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,24 @@
 class Book
 # write your code here
+attr_accessor :title
+
+	def initialize(title = "random title")
+		@title = title
+
+	end
+
+	def title
+		 small_words = %w[on the and in of a an]
+		  @title.split(' ').map.with_index do |w, i|
+		    unless (small_words.include? w) and (i > 0)
+		      w.capitalize
+		    else
+		      w
+		    end
+		  end.join(' ')
+		
+		 
+	end
+
+
 end
