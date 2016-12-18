d09f60b4756c8d2e6ee1227c7c85bd672c215714
jayherring <j.herring.8989@gmail.com>
2016-12-17 18:20:18 -0600

    helper function

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,27 @@
 class Timer
   #write your code here
+  attr_accessor :seconds
+
+  def intialize
+  	@seconds = 0
+  end
+
+  def seconds
+  	@seconds = 0
+  end
+
+  def padding(number)
+  	number.to_s.rjust(2,"0")
+  end
+
+
+  def time_string 
+  	 hours = (@seconds /3600)
+  	 minutes = (@seconds- hours *3600) / 60
+  	 seconds = (@seconds- hours *3600 - minutes * 60)
+
+  	 "#{padding(hours) + ':' + padding(minutes) + ':' + padding(seconds)}"
+  end
+
+
 end
